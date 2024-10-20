157cbeec0e00656c52b50bfea612ea0d845436d3
Soumik <soumiksaha4223@gmail.com>
2024-10-21 04:41:39 +0530

    demo update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      Start your work for Codearena landing page
+      Start creating your landing page
     </>
   );
 }
