68e390aa04fd48920aa1ac9257f3f65f292b8b48
Soumik Singha Roy <soumiksingharoy41@gmail.com>
2024-10-21 23:15:56 +0530

    landing page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-     <h1>i am on my way</h1>
+     <h1>i am on my way.</h1>
+     <h1>soumik</h1>
     </>
   );
 }
